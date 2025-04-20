59eac62ef03c000aedc24e332c1e92def3a70050
CRVX <benlazregahmed328@gmail.com>
2025-04-20 17:32:37 +0100

    formatting main.tf

--- hello-world/web-app/main.tf
+++ hello-world/web-app/main.tf
@@ -1,12 +1,12 @@
 terraform {
-  cloud { 
-    
-    organization = "ISIMM-TEST" 
+  cloud {
 
-    workspaces { 
-      name = "gh-actions" 
-    } 
-  }   
+    organization = "ISIMM-TEST"
+
+    workspaces {
+      name = "gh-actions"
+    }
+  }
 
   required_providers {
     aws = {
@@ -21,9 +21,9 @@ provider "aws" {
 }
 
 module "web-app-module" {
-  source = "./modules"
-  app_name = "web-app"
-  ami = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+  source        = "./modules"
+  app_name      = "web-app"
+  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
   instance_type = "t2.micro"
-  region = "us-east-1"
+  region        = "us-east-1"
 }
